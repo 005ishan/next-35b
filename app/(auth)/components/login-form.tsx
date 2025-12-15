281d03395c8577d646489ab464d1399518d6de5b
0050505ishan <isan91798@gmail.com>
2025-12-15 17:54:57 +0545

    commit

---
+++ app/(auth)/components/login-form.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function LoginForm() {
+    return (
+        <   div className="ax-auto max-w-md border m-2 p-4 rounded items-center text-center">
+            <div className="m-1">
+                <label className="block mb-2 font-semibold">Email</label>
+                <input className="p-1 border"/>
+            </div>
+            <div className="m-1">
+                <label className="block m-2 font-semibold">Password</label>
+                <input type="password" className="p-1 border"/>
+            </div>
+            <button className="bg-green-500 border rounded p-2 mt-2 text-white"></button>
+            <div>
+                Don't have an account?
+                <Link href="/register" className="text-blue-500 unnderline">Register</Link>
+            </div>
+        </div>
+    );
+}
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function Layout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            head
+            {children}
+            @2025
+        </section>
+    );
+}
